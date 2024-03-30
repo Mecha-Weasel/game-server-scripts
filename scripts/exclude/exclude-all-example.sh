2bcdd7bdd2c8558364a4070a771df97acd172b2e
Weasel <Weasel.SteamID.155+GitHub@gMail.com>
2024-03-29 21:07:39 -0700

    Fixed issues with "exclude-all-example" script
    Fixed issues with "exclude-all-example" script accidentally calling "start" scripts for each server, instead of the "exclude" script for each server.

--- scripts/exclude/exclude-all-example.sh
+++ scripts/exclude/exclude-all-example.sh
@@ -4,7 +4,7 @@
 #	Update backup exclusion files for all game-servers
 #	============================================================================
 #	Created:       2024-03-07, by Weasel.SteamID.155@gMail.com        
-#	Last modified: 2024-03-23, by Weasel.SteamID.155@gMail.com
+#	Last modified: 2024-03-29, by Weasel.SteamID.155@gMail.com
 #	----------------------------------------------------------------------------
 #
 #	Define some variables ...
@@ -17,10 +17,10 @@ echo "[Start of script: ${0##*/}]";
 #
 #	Call the standard exclude scripts ...
 #
-#$SCRIPTS_FOLDER/start/start-gameserverid1.sh;	#	example game server #1
-#$SCRIPTS_FOLDER/start/start-gameserverid2.sh;	#	example game server #2
-#$SCRIPTS_FOLDER/start/start-gameserverid3.sh;	#	example game server #3
-#$SCRIPTS_FOLDER/start/start-gameserverid4.sh;	#	example game server #4
+#$SCRIPTS_FOLDER/exclude/exclude-gameserverid1.sh;	#	example game server #1
+#$SCRIPTS_FOLDER/exclude/exclude-gameserverid2.sh;	#	example game server #2
+#$SCRIPTS_FOLDER/exclude/exclude-gameserverid3.sh;	#	example game server #3
+#$SCRIPTS_FOLDER/exclude/exclude-gameserverid4.sh;	#	example game server #4
 #
 #	Display end of stuff ...
 #
